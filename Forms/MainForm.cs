16913cf2b0224b52995d55c0cba6ae1d0622bb58
Andreas Aakvik Gogstad <agogstad@gmail.com>
2026-09-25 12:52:52 +0200

    version

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Text;
 
-[assembly: AssemblyVersion("2.6.*")]
+[assembly: AssemblyVersion("2.7.*")]
 
 #pragma warning disable IDE0090 // Use 'new(...)'
 
